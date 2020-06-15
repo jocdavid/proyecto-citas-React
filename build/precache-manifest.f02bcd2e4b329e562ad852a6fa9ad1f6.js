2300f591f52ebb59109a9ef22160a44a1a7973d5
jocdavid <jflorez1223@gmail.com>
2020-06-14 19:22:59 -0500

    Add files via upload

---
+++ build/precache-manifest.f02bcd2e4b329e562ad852a6fa9ad1f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3cc611515141a2d7ad31f18df40bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3875bb944091b831876d",
+    "url": "/static/css/main.39a6cfe0.chunk.css"
+  },
+  {
+    "revision": "66bc1455e1bcb36af355",
+    "url": "/static/js/2.917c2d09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.917c2d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3875bb944091b831876d",
+    "url": "/static/js/main.69b3c60b.chunk.js"
+  },
+  {
+    "revision": "1b13ee7f0369ce8df4c9",
+    "url": "/static/js/runtime-main.4dc01cc0.js"
+  }
+]);
